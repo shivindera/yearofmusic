5dfe662b1c014aeb3fe784d9feee2a9582c625ce
Shivindera Singh <shivindera.singh@booking.com>
2017-12-31 19:13:11 +0100

    add all the files

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+	, logger = require('morgan')
+	, app = express()
+	, template = require('jade').compileFile(__dirname + '/templates/homepage.jade')
+
+app.use(logger('dev'))
+app.use('/img', express.static(__dirname + '/static/img'))
+app.use('/css', express.static(__dirname + '/static/css'))
+app.use('/js', express.static(__dirname + '/static/js'))
+app.use('/json', express.static(__dirname + '/static/json'))
+
+app.get('/', function (req, res, next) {
+	try {
+		var html = template()
+		res.send(html)
+	} catch (e) {
+		next(e)
+	}
+})
+
+app.listen(process.env.PORT || 3000, function () {
+	console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
---
+++ static/js/index.js
@@ -0,0 +1,28 @@
+var monthNames = [
+	"Jan", "Feb", "Mar",
+	"Apr", "May", "Jun", "Jul",
+	"Aug", "Sep", "Oct",
+	"Nov", "Dec"
+];
+
+var BreakException = {};
+
+$(document).ready(function () {
+	$.getJSON('/json/list.json', function (json) {
+		var now = new Date();
+		var d = new Date(2018,0,1);
+		try {
+			json.forEach(function (val) {
+				if (Math.ceil((now.getTime() - d.getTime()) / (1000 * 3600 * 24)) <= 1) {
+					var vid = val.l.substring(val.l.indexOf('?v=') + 3);
+					$('.videoWrapper').append("<iframe id='ytplayer' src='https://www.youtube.com/embed/" + vid + "?autoplay=1' frameborder='0'></iframe>");
+					throw BreakException;
+				}
+				$('.list').append("<li><span>" + monthNames[d.getMonth()] + " " + d.getDate() + "</span><a href=" + val.l + ">" + val.t + "</span></li>");
+				d.setDate(d.getDate() + 1);
+			});
+		} catch (e) {
+			if (e !== BreakException) throw e;
+		}
+	});
+});
