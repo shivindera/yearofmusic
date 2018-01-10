e44a13ddcc66925f950784bfdaa8486e4ae1d9d2
Shivindera Singh <shivindera.singh@booking.com>
2018-01-10 14:35:03 +0100

    fix issue with favicon

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express')
 	, logger = require('morgan')
+	, favicon = require('serve-favicon')
+	, path = require('path')
 	, app = express()
 	, template = require('jade').compileFile(__dirname + '/templates/homepage.jade')
 
 app.use(logger('dev'))
+app.use(favicon(path.join(__dirname, '/static/img', 'favicon.ico')))
 app.use('/img', express.static(__dirname + '/static/img'))
 app.use('/css', express.static(__dirname + '/static/css'))
 app.use('/js', express.static(__dirname + '/static/js'))
